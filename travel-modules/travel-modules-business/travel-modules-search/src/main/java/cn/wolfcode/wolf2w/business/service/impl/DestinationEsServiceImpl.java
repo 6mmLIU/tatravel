f02d16e3a3e402374046fe1ea2e0eda259fc1abb
Wanderlnk <125452745+6mmLIU@users.noreply.github.com>
2025-08-05 16:26:14 +0800

    feat: handle remote data initialization failures

--- travel-modules/travel-modules-business/travel-modules-search/src/main/java/cn/wolfcode/wolf2w/business/service/impl/DestinationEsServiceImpl.java
+++ travel-modules/travel-modules-business/travel-modules-search/src/main/java/cn/wolfcode/wolf2w/business/service/impl/DestinationEsServiceImpl.java
@@ -23,10 +23,13 @@ public class DestinationEsServiceImpl implements IDestinationEsService {
 
     @Override
     public void initData() {
-        R<List<Destination>> list = remoteDestinationService.list2(SecurityConstants.INNER);
-        log.info(list.getMsg()+"异常");
+        R<List<Destination>> result = remoteDestinationService.list2(SecurityConstants.INNER);
+        if (R.isError(result) || result.getData() == null) {
+            log.warn("获取目的地数据失败:{}", result.getMsg());
+            return;
+        }
         List<DestinationES> esList = new ArrayList<>();
-        for (Destination destination : list.getData()) {
+        for (Destination destination : result.getData()) {
             DestinationES es = new DestinationES();
             BeanUtils.copyProperties(destination, es);
             esList.add(es);
